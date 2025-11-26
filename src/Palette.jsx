5cfddbc6a4b61be6a5de499e22c77e03a22140ec
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-11-26 18:00:22 +0100

    Style Slider

--- src/Palette.jsx
+++ src/Palette.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import ColorBox from "./ColorBox";
-import "./Palette.css";
 import Slider from "rc-slider";
 import 'rc-slider/assets/index.css';
+import "./Palette.css";
 
 function Palette({palette}){
     const [level, setLevel] = useState(500);
@@ -15,13 +15,15 @@ function Palette({palette}){
     }
     return(
         <div className="Palette">
-            <Slider
-              defaultValue={level}
-              min={100}
-              max={900}
-              step={100}
-              onChangeComplete={changeLevel}
-            />
+            <div className="slider">
+                <Slider
+                  defaultValue={level}
+                  min={100}
+                  max={900}
+                  step={100}
+                  onChangeComplete={changeLevel}
+                />
+            </div>
             <div className="Palette-colors">
                 {colorBoxes}
             </div>
