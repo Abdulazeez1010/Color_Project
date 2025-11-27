3edc147b6aa46070136d017b753c06a2176988a8
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-11-27 17:53:38 +0100

    Add snackbar and Material icons

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,16 +2,26 @@
 import { useState } from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+
+import CloseIcon from '@mui/icons-material/Close';
 import Slider from "rc-slider";
 import 'rc-slider/assets/index.css';
 import './Navbar.css'
 
 function Navbar({level, changeLevel, handleChange}){
     const [format, setFormat] = useState("hex");
+    const [open, setOpen] = useState(false);
 
-    const handleChangeFormat = (e) =>{
+    const handleFormatChange = (e) =>{
         setFormat(e.target.value);
+        setOpen(true)
         handleChange(e.target.value)
+    }
+
+    const closeSnackbar = () =>{
+        setOpen(false);
     }
 
     return(
@@ -32,12 +42,29 @@ function Navbar({level, changeLevel, handleChange}){
                 </div>
             </div>
             <div className="select-container">
-                <Select value={format} onChange={handleChangeFormat}>
+                <Select value={format} onChange={handleFormatChange}>
                     <MenuItem value="hex">HEX - #ffffff</MenuItem>
                     <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
                     <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
                 </Select>
             </div>
+            <Snackbar
+            //   anchorOrigin={{vertical: "bottom", horizontal: "left"}}
+              open={open}
+              autoHideDuration={3000}
+              onClose={closeSnackbar}
+              message={<span>Format Changed to {format.toUpperCase()}</span>}
+              action={[
+                <IconButton
+                  onClick={closeSnackbar}
+                  color="inherit"
+                  key="close"
+                  aria-label="close"
+                >
+                    <CloseIcon />
+                </IconButton>
+              ]}
+            />
         </header>
     )
 }
