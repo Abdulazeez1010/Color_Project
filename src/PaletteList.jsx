d7bd11bf0af8688aa2300cd7eace56ca74f13305
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-11-28 15:43:05 +0100

    Start styling PaletteList with MUI styled

--- src/PaletteList.jsx
+++ src/PaletteList.jsx
@@ -1,15 +1,58 @@
 
+import { styled } from "@mui/material/styles";
 import MiniPalette from "./MiniPalette";
 import { Link } from "react-router-dom";
 
+const Root = styled("div")({
+    backgroundColor: "blue",
+    height: "100%",
+    display: "flex",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  
+    "&:hover": {
+        cursor: "pointer"
+    }
+});
+
+const Container = styled("div")({
+    width: "50%",
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    flexWrap: "wrap"
+});
+
+const Nav = styled("nav")({
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    color: "white"
+});
+
+const Palettes = styled("div")({
+    boxSizing:"border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%"
+})
+
 function PaletteList({palettes}){
     return(
-        <div>
-            <h1>Palette List</h1>
-            {palettes.map(palette => (
-                <MiniPalette {...palette}/>
-                ))}
-        </div>
+        <Root>
+            <Container>
+                <Nav>
+                     <h1>React Colors</h1>
+                </Nav>
+                <Palettes>
+                    {palettes.map(palette => (
+                        <MiniPalette {...palette}/>
+                        ))}
+                </Palettes>
+
+            </Container>
+        </Root>
     )
 }
 
