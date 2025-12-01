b6d4f964e1382963ec930ba7df0187ab56bc3dce
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-12-01 15:30:07 +0100

    Finish refactor of ColorBox CSS

--- src/ColorBox.jsx
+++ src/ColorBox.jsx
@@ -18,16 +18,16 @@ const ColorBoxDiv = styled("div")((props) => ({
         transition: "0.5s"
     }
 }))
-const CopyText = styled("p")((props) => ({
-    color: chroma(props.background).luminance() >= 0.7 ? "black" : "white"
+const CopyText = styled("p")(({background}) => ({
+    color: chroma(background).luminance() >= 0.7 ? "black" : "white"
 }))
 
-const ColorName = styled("span")((props) => ({
-    color: chroma(props.background).luminance() <= 0.08 ? "white": "black"
+const ColorName = styled("span")(({background}) => ({
+    color: chroma(background).luminance() <= 0.08 ? "white": "black"
 }))
 
-const SeeMore = styled("span")((props) => ({
-    color: chroma(props.background).luminance() >= 0.7 ? "rgba(0,0,0,0.6)" : "white",
+const SeeMore = styled("span")(({background}) => ({
+    color: chroma(background).luminance() >= 0.7 ? "rgba(0,0,0,0.6)" : "white",
     background: "rgba(255, 255, 255, 0.3)",
     position: "absolute",
     border: "none",
@@ -40,8 +40,8 @@ const SeeMore = styled("span")((props) => ({
     textTransform: "uppercase"
 }))
 
-const CopyButton = styled("button")((props) => ({
-    color: chroma(props.background).luminance() >= 0.7 ? "rgba(0,0,0,0.6)" : "white",
+const CopyButton = styled("button")(({background}) => ({
+    color: chroma(background).luminance() >= 0.7 ? "rgba(0,0,0,0.6)" : "white",
     width: "100px",
     height: "30px",
     position: "absolute",
@@ -60,6 +60,64 @@ const CopyButton = styled("button")((props) => ({
     textDecoration: "none",
     opacity: "0"
 }))
+
+const BoxContent = styled("div")({
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "black",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px"
+})
+
+const CopyOverlay = styled("div")(({copied, background}) => ({
+    background,
+    opacity: copied ? 1 : 0,
+    zIndex: copied ? 10 : 0,
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    transition: "transform 0.6s ease-in-out",
+    transform: copied ? "scale(50)" : "scale(0.1)"
+}))
+
+const CopyMessage = styled("div")(({copied}) => ({
+    position: "fixed",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    fontSize: "4rem",
+    transform: copied ? "scale(1)" : "scale(0.1)",
+    opacity: copied ? 1 : 0,
+    color: "white",
+    zIndex: copied ? 25 : 0,
+    transition: copied ? "all 0.4s ease-in-out" : "",
+    transitionDelay: copied ? "0.3s" : "",
+    "& h1": {
+        fontWeight: 400,
+        textShadow: "1px 2px black",
+        background: "rgba(255, 255, 255, 0.2)",
+        width: "100%",
+        textAlign: "center",
+        marginBottom: 0,
+        padding: "1rem",
+        textTransform: "uppercase"
+    },
+    "& p": {
+        fontSize: "2rem",
+        fontWeight: 100
+    }
+}))
+
+
 
 
 function ColorBox({background, name, paletteId, id, showingFullPalette}){    
@@ -81,15 +139,15 @@ function ColorBox({background, name, paletteId, id, showingFullPalette}){
             style={{background: background}}
             showingFullPalette = {showingFullPalette}
         >
-        <div style={{background}} className={`copy-overlay ${copied && "show"}`}/>
-        <div className={`copy-msg ${copied && "show"}`}>
+        <CopyOverlay copied={copied} background={background}/>
+        <CopyMessage  copied={copied}>
             <h1>copied!</h1>
             <CopyText background={background}>{background}</CopyText>
-        </div>
-            <div className='copy-contianer'>
-                <div className="box-content">
+        </CopyMessage>
+            <div>
+                <BoxContent>
                     <ColorName background={background}>{name}</ColorName>
-                </div>
+                </BoxContent>
                 <CopyButton background={background}> Copy </CopyButton>
             </div>
             {showingFullPalette && (
