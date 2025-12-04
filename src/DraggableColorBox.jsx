94959830fc640299ae37ff64ece22c3ad0c3c83c
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-12-04 10:37:55 +0100

    Style DraggableColorBox

--- src/DraggableColorBox.jsx
+++ src/DraggableColorBox.jsx
@@ -1,5 +1,7 @@
 
 import { styled } from "@mui/material/styles";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { colors } from "@mui/material";
 
 const Root = styled("div")({
     width: "20%",
@@ -8,13 +10,35 @@ const Root = styled("div")({
     display: "inline-block",
     position: "relative",
     cursor: "pointer",
-    marginBottom: "-5px"
-})
+    marginBottom: "-5px",
+    "&:hover svg": {
+        color: "white",
+        transform: "scale(1.5)"
+    }
+});
+
+const BoxContent = styled("div")({
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "rgba(0, 0, 0, 0.5)",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between"
+});
+
 
 function DraggableColorbox({color, name}){
     return (
         <Root style={{backgroundColor: color}}>
-            {name}
+            <BoxContent>
+                <span>{name}</span>
+                <DeleteIcon sx={{transition: "all 0.3s ease-in-out"}}/>
+            </BoxContent>
         </Root>
     )
 }
