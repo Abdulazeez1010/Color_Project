d439938aa1f95d19377a34148d05ba8cc32aabf4
Abdulazeez1010 <alani.abdulazeez.akinpelumi@gmail.com>
2025-12-07 16:41:45 +0100

    Save palettes to LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import PaletteRouter from './PaletteRouter';
 import PaletteList from './PaletteList';
@@ -9,13 +9,18 @@ import seedColors from './seedColors';
 
 function App() {
 
-  const [palettes, setPalette] = useState(seedColors);
+  const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
 
+  const [palettes, setPalette] = useState(savedPalettes || seedColors);
+
+  useEffect(() => {
+    window.localStorage.setItem("palettes", JSON.stringify(palettes))
+  }, [palettes]);
 
   const savePalette = (newPalette) => {
-    setPalette([...palettes, newPalette])
+    setPalette(prev => [...prev, newPalette])
   }
-  
+
   return (
     <div>
       <Routes>
